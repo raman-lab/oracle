414d3297d230a674b605fd122d671c38e01ecea2
PhilHuss <74377240+PhilHuss@users.noreply.github.com>
2021-03-01 15:35:48 -0600

    Update map_physicochemical_properties.R

--- physicochemical/scripts/map_physicochemical_properties.R
+++ physicochemical/scripts/map_physicochemical_properties.R
@@ -1,74 +1,145 @@
 #Overview of this script:
-    #Computes the changes in physicochemical properties for all single mutants of deep mutational scan
-    #Relates experimentally determined DMS functional scores to changes in physicochemical properties
-    #Generates violin plots to identify global trends in host-specific enrichment of single mutants
+#Computes the changes in physicochemical properties for all single mutants of deep mutational scan
+#Relates experimentally determined DMS functional scores to changes in physicochemical properties
+#Generates violin plots to identify global trends in host-specific enrichment of single mutants
+
+library(ggplot2)
+library(ggpubr)
 
 #Read in the file that contains table of physicochemical properties of each amino acid 
-pchem    <- read.csv("../reference/AA_physicochemical_properties.csv", header=T, stringsAsFactors=F)
-#Read in the file that contains the table of experimentally determined DMS functional scores
-fscore   <- read.csv("../reference/DMS_Fscores.csv", header=T, stringsAsFactors=F)
+
+d <- read.csv("../analysis/delta_pchem.csv", header=T, stringsAsFactors=F, na.strings="NA")
 
 #Compute the change in each physicochemical property for all single mutants 
 #----------------------------------------------------------------------------------------------
-for(i in 1:length(fscore[,1]))
-{  
-  wt  <- subset(pchem, pchem[,1]==substr(fscore[i,1],1,1))
-  mut <- subset(pchem, pchem[,1]==substr(fscore[i,3],1,1))
-  
-  for(property in 1:9)
-  {
-    fscore[i,property+8] <- mut[1,property+1] - wt[1,property+1]
-  }
-}
-for(property in 1:9)
+
+for(i in 14:18)
 {
-  colnames(fscore)[property+8] <- paste0("delta_",colnames(pchem)[property+1])
+  print(colnames(d)[i])
+  ss <- subset(d, is.na(d[,i]))
+  print(length(ss[,1]))
 }
-write.csv(fscore, "../analysis/delta_physicochemical_properties.csv", row.names=F, quote=F)
+d <- subset(d, !is.na(d[,14]))
+
 #----------------------------------------------------------------------------------------------
 
 #Based on Fscores, assign a phenotype ("depleted", "Tolerated", or "Enriched") to each single mutant 
 #----------------------------------------------------------------------------------------------
-pheno <- as.data.frame(matrix(0,1,11))
-colnames(pheno)[1:9] <- colnames(fscore)[9:17]
-colnames(pheno)[10:11] <- c("strain","group")
 
-c <- 0
-for(strain in 1:5)
+
+x10g <- d[,c(1:14)]
+x10g[,15] <- "10G"
+colnames(x10g)[14:15] <- c('fscore','strain')
+
+bl21 <- d[,c(1:13,15)]
+bl21[,15] <- "BL21"
+colnames(bl21)[14:15] <- c('fscore','strain')
+
+bw25113 <- d[,c(1:13,16)]
+bw25113[,15] <- "BW25113"
+colnames(bw25113)[14:15] <- c('fscore','strain')
+
+rfad <- d[,c(1:13,17)]
+rfad[,15] <- "rfaD"
+colnames(rfad)[14:15] <- c('fscore','strain')
+
+rfag <- d[,c(1:13,18)]
+rfag[,15] <- "rfaG"
+colnames(rfag)[14:15] <- c('fscore','strain')
+
+for(i in 1:length(d[,1]))
 {
-  for(i in 1:length(fscore[,1]))
+  if(x10g[i,14] <= 0.1)
   {
-    c <- c + 1
-    pheno[c,1:9] <- fscore[i,9:17]
-    pheno[c,10] <- colnames(fscore)[3+strain]
-    if(is.na(fscore[i,3+strain]))
+    x10g[i,16] <- 'Depleted'
+  } else
+  {
+    if(x10g[i,14] < 2)
     {
-      pheno[c,11] <- NA
-    } 
-    else if(fscore[i,3+strain] < 0.1)
+      x10g[i,16] <- 'Tolerated'
+    } else
     {
-      pheno[c,11] <- "Depleted"
-    } 
-    else if(fscore[i,3+strain] < 10)
+      x10g[i,16] <- 'Enriched'
+    }
+  }
+  
+  if(bl21[i,14] <= 0.1)
+  {
+    bl21[i,16] <- 'Depleted'
+  } else
+  {
+    if(bl21[i,14] < 2)
     {
-      pheno[c,11] <- "Tolerated"
-    } 
-    else
+      bl21[i,16] <- 'Tolerated'
+    } else
     {
-      pheno[c,11] <- "Enriched"
+      bl21[i,16] <- 'Enriched'
+    }
+  }
+  
+  if(bw25113[i,14] <= 0.1)
+  {
+    bw25113[i,16] <- 'Depleted'
+  } else
+  {
+    if(bw25113[i,14] < 2)
+    {
+      bw25113[i,16] <- 'Tolerated'
+    } else
+    {
+      bw25113[i,16] <- 'Enriched'
+    }
+  }
+  
+  if(rfad[i,14] <= 0.1)
+  {
+    rfad[i,16] <- 'Depleted'
+  } else
+  {
+    if(rfad[i,14] < 10)
+    {
+      rfad[i,16] <- 'Tolerated'
+    } else
+    {
+      rfad[i,16] <- 'Enriched'
+    }
+  }
+  
+  if(is.na(rfag[i,14]))
+  {
+    rfag[i,16] <- 'NA'
+  } else
+  {
+    if(rfag[i,14] <= 0.1)
+    {
+      rfag[i,16] <- 'Depleted'
+    } else
+    {
+      if(rfag[i,14] < 10)
+      {
+        rfag[i,16] <- 'Tolerated'
+      } else
+      {
+        rfag[i,16] <- 'Enriched'
+      }
     }
   }
 }
+
+global <- rbind(x10g, bl21, bw25113, rfad, rfag)
+colnames(global)[16] <- 'group'
+
 #----------------------------------------------------------------------------------------------
 
 #Generate violin plots to identify global trends in host-specific enrichment of single mutants
 #----------------------------------------------------------------------------------------------
-library(ggplot2)
-library(ggpubr)
+
 my_comparisons <- list(c("Depleted", "Tolerated"), c("Tolerated", "Enriched"), c("Depleted", "Enriched"))
-pheno <- subset(pheno, !is.na(pheno[,11]))
-pdf(file="../analysis/pdfs/violin_delta_hydrophobicity.pdf", width=12, height=4.5, useDingbats=F)
-p <- ggviolin(pheno, x="group", y="delta_hydrophobicity", facet.by="strain", short.panel.labs=T, 
+global[,15] <- as.factor(global[,15])
+global[,16] <- as.factor(global[,16])
+
+pdf(file="global/violin_hydrophobicity.pdf", width=12, height=4.5, useDingbats=F)
+p <- ggviolin(global, x="group", y="hydrophobicity", facet.by="strain", short.panel.labs=T, 
               add.params = list(fill = "white"), fill="group", 
               palette=c("#c2a5cf","gray90","#a6dba0"), add="boxplot", repel=T,
               ylab=expression(paste(Delta,Hydrophobicity)), xlab="", ylim=c(-5,7.5),
@@ -76,8 +147,8 @@ p <- ggviolin(pheno, x="group", y="delta_hydrophobicity", facet.by="strain", sho
 p + stat_compare_means(method="kruskal.test", label="p.format", label.x=1.6, label.y=7.3) + stat_compare_means(comparisons=my_comparisons) + theme_classic() + facet_grid(. ~ strain) + theme(legend.position="none", text=element_text(size=20), axis.text.x=element_text(angle=45, colour="black", vjust=0.5), axis.text.y=element_text(colour="black"))
 dev.off()
 
-pdf(file="../analysis/pdfs/violin_delta_hydrophilicity.pdf", width=12, height=4.5, useDingbats=F)
-p <- ggviolin(pheno, x="group", y="delta_hydrophilicity", facet.by="strain", short.panel.labs=T, 
+pdf(file="global/violin_hydrophilicity.pdf", width=12, height=4.5, useDingbats=F)
+p <- ggviolin(global, x="group", y="hydrophilicity", facet.by="strain", short.panel.labs=T, 
               add.params = list(fill = "white"), fill="group", 
               palette=c("#c2a5cf","gray90","#a6dba0"), add="boxplot", repel=T,
               ylab=expression(paste(Delta,Hydrophilicity)), xlab="", ylim=c(-10,13),
@@ -85,8 +156,8 @@ p <- ggviolin(pheno, x="group", y="delta_hydrophilicity", facet.by="strain", sho
 p + stat_compare_means(method="kruskal.test", label="p.format", label.x=1.6, label.y=12.5) + stat_compare_means(comparisons=my_comparisons) + theme_classic() + facet_grid(. ~ strain) + theme(legend.position="none", text=element_text(size=20), axis.text.x=element_text(angle=45, colour="black", vjust=0.5), axis.text.y=element_text(colour="black"))
 dev.off()
 
-pdf(file="../analysis/pdfs/violin_delta_polarity.pdf", width=12, height=4.5, useDingbats=F)
-p <- ggviolin(pheno, x="group", y="delta_polarity", facet.by="strain", short.panel.labs=T, 
+pdf(file="global/violin_polarity.pdf", width=12, height=4.5, useDingbats=F)
+p <- ggviolin(global, x="group", y="polarity", facet.by="strain", short.panel.labs=T, 
               add.params = list(fill = "white"), fill="group", 
               palette=c("#c2a5cf","gray90","#a6dba0"), add="boxplot", repel=T,
               ylab=expression(paste(Delta,Polarity)), xlab="", ylim=c(-12,16),
@@ -94,8 +165,8 @@ p <- ggviolin(pheno, x="group", y="delta_polarity", facet.by="strain", short.pan
 p + stat_compare_means(method="kruskal.test", label="p.format", label.x=1.6, label.y=15.5) + stat_compare_means(comparisons=my_comparisons) + theme_classic() + facet_grid(. ~ strain) + theme(legend.position="none", text=element_text(size=20), axis.text.x=element_text(angle=45, colour="black", vjust=0.5), axis.text.y=element_text(colour="black"))
 dev.off()
 
-pdf(file="../analysis/pdfs/violin_delta_polarizability.pdf", width=12, height=4.5, useDingbats=F)
-p <- ggviolin(pheno, x="group", y="delta_polarizability", facet.by="strain", short.panel.labs=T, 
+pdf(file="global/violin_polarizability.pdf", width=12, height=4.5, useDingbats=F)
+p <- ggviolin(global, x="group", y="polarizability", facet.by="strain", short.panel.labs=T, 
               add.params = list(fill = "white"), fill="group", 
               palette=c("#c2a5cf","gray90","#a6dba0"), add="boxplot", repel=T,
               ylab=expression(paste(Delta,Polarizability)), xlab="", ylim=c(-0.5,0.8),
@@ -103,14 +174,15 @@ p <- ggviolin(pheno, x="group", y="delta_polarizability", facet.by="strain", sho
 p + stat_compare_means(method="kruskal.test", label="p.format", label.x=1.6, label.y=0.78) + stat_compare_means(comparisons=my_comparisons) + theme_classic() + facet_grid(. ~ strain) + theme(legend.position="none", text=element_text(size=20), axis.text.x=element_text(angle=45, colour="black", vjust=0.5), axis.text.y=element_text(colour="black"))
 dev.off()
 
-pdf(file="../analysis/pdfs/violin_delta_mass.pdf", width=12, height=4.5, useDingbats=F)
-p <- ggviolin(pheno, x="group", y="delta_mass", facet.by="strain", short.panel.labs=T, 
+pdf(file="global/violin_mass.pdf", width=12, height=4.5, useDingbats=F)
+p <- ggviolin(global, x="group", y="mass", facet.by="strain", short.panel.labs=T, 
               add.params = list(fill = "white"), fill="group", 
               palette=c("#c2a5cf","gray90","#a6dba0"), add="boxplot", repel=T,
               ylab=expression(paste(Delta,Mass (g/mol))), xlab="", ylim=c(-180,250),
               order=c("Depleted","Tolerated","Enriched"))
 p + stat_compare_means(method="kruskal.test", label="p.format", label.x=1.6, label.y=242) + stat_compare_means(comparisons=my_comparisons) + theme_classic() + facet_grid(. ~ strain) + theme(legend.position="none", text=element_text(size=20), axis.text.x=element_text(angle=45, colour="black", vjust=0.5), axis.text.y=element_text(colour="black"))
 dev.off()
+
 #----------------------------------------------------------------------------------------------
 
 #Wilcoxon effect size comparisons
@@ -120,6 +192,7 @@ dev.off()
 
 #install.packages("rcompanion")
 library(rcompanion)
+
 #install.packages("rstatix", dependencies = T)
 library(rstatix)
 
